10ee1338e28cb66f87cf5fb67cd3e3b7f7d0a790
tony.astolfi <tony.astolfi@tamr.com>
2017-12-02 11:23:08 -0500

    solved part 2 of day 2

--- day-2/main.cc
+++ day-2/main.cc
@@ -2,12 +2,14 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 int main() {
   string line;
   long checksum = 0;
+  long quotient_sum = 0;
   while (cin.good()) {
     getline(cin, line);
     istringstream iss(line);
@@ -17,6 +19,7 @@ int main() {
       continue;
     }
     long line_max = line_min;
+    vector<long> nums = {line_min};
     while (iss.good()) {
       long next;
       iss >> next;
@@ -25,9 +28,18 @@ int main() {
       }
       line_min = min(line_min, next);
       line_max = max(line_max, next);
+      for (long n : nums) {
+        long num = max(n, next);
+        long div = min(n, next);
+        if (num % div == 0) {
+          quotient_sum += num / div;
+        }
+      }
+      nums.push_back(next);
     }
     checksum += line_max - line_min;
   }
-  cout << checksum << endl;
+  cout << "part-1: " << checksum << endl;
+  cout << "part-2: " << quotient_sum << endl;
   return 0;
 }
